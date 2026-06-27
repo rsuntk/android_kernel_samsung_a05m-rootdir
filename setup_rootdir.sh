400a820070a93093da5b60988099a6f3ca2dd3c5
ris <rissu.ntk@gmail.com>
2026-06-27 16:58:54 +0700

    a05m: fixup mgk
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- setup_rootdir.sh
+++ setup_rootdir.sh
@@ -17,16 +17,17 @@ clone_quiet() {
     local branch="$2"
     local dest="$3"
 
-    echo "[$dest] Cloning branch $branch .."
+    echo "Cloning branch $branch [$dest] .."
     git clone -q --depth=1 --single-branch -b $branch $url $dest
 }
 
 # grab kernel
 clone_quiet https://github.com/rsuntk/android_kernel_samsung_a05m-6.6.git android15-6.6-lts kernel-6.6
 clone_quiet https://github.com/rsuntk/android_kernel_device_modules-6.6.git master kernel/kernel_device_modules-6.6
-clone_quiet https://github.com/rsuntk/android_vendor_mediatek_kernel_modules.git master kernel/vendor/mediatek/kernel_modules
+clone_quiet https://github.com/rsuntk/android_vendor_mediatek_kernel_modules.git master vendor/mediatek/kernel_modules
 
 # append tc
+echo "Appending and Extracting toolchains into $(pwd) .."
 cat toolchains.tar.gz_00 toolchains.tar.gz_01 > tc.gz
 tar -xf tc.gz
 rm toolchains.tar* tc*gz
