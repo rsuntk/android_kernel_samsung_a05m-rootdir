ded78a02486226e8ad2613f1560f1035e81860ac
ris <rissu.ntk@gmail.com>
2026-06-27 13:46:13 +0700

    a05m: fixup setup
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- setup_codespace.sh
+++ setup_codespace.sh
@@ -6,7 +6,7 @@ rm -rf .git
 
 # grab deps
 sudo apt update -y
-sudo apt install aria2c byobu -y
+sudo apt install aria2 byobu -y
 
 # grab tc
 aria2c https://github.com/rsuntk/files/releases/download/a05m-tc/toolchains.tar.gz_00
@@ -16,7 +16,7 @@ aria2c https://github.com/rsuntk/files/releases/download/a05m-tc/toolchains.tar.
 OPT_FLAGS="-q --depth=1 --single-branch"
 BRANCH="android15-6.6-lts"
 echo "Cloning branch $BRANCH .."
-git clone OPT_FLAGS https://github.com/rsuntk/android_kernel_samsung_a05m-6.6.git kernel-6.6 -b $BRANCH
+git clone $OPT_FLAGS https://github.com/rsuntk/android_kernel_samsung_a05m-6.6.git kernel-6.6 -b $BRANCH
 
 # append tc
 cat toolchains.tar.gz_00 toolchains.tar.gz_01 > tc.gz
