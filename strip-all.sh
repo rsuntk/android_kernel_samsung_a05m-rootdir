a607b356f441f80dfc478a14f67ba710a7ee71ea
ris <rissu.ntk@gmail.com>
2026-07-03 14:59:30 +0700

    strip-all: fixup!
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- strip-all.sh
+++ strip-all.sh
@@ -1,3 +1,4 @@
-for i in "$(find . -type f -name "*.ko")"; do
+files=$(find . -type f -name "*.ko")
+for i in $files; do
 	$(pwd)/prebuilts/clang/host/linux-x86/clang-r510928/bin/llvm-strip --strip-unneeded $i
 done
