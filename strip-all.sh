92677602c37152b4e2c78daeba3d410754362f4d
ris <rissu.ntk@gmail.com>
2026-07-06 21:36:41 +0700

    strip-all: fix
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- strip-all.sh
+++ strip-all.sh
@@ -1,4 +1,5 @@
 files=$(find . -type f -name "*.ko")
 for i in $files; do
-	$(pwd)/prebuilts/clang/host/linux-x86/clang-r510928/bin/llvm-strip --strip-unneeded $i
+    $(pwd)/prebuilts/clang/host/linux-x86/clang-r510928/bin/llvm-strip --strip-unneeded "$i"
+    echo "Stripping $i .."
 done
