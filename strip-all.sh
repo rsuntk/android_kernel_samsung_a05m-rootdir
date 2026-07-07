4c1ba24e125490f12ec191e2f786aa6f29d12241
ris <rissu.ntk@gmail.com>
2026-07-07 08:45:09 +0700

    strip-all: Only iterate out dir
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- strip-all.sh
+++ strip-all.sh
@@ -1,4 +1,4 @@
-files=$(find . -type f -name "*.ko")
+files=$(find ./out -type f -name "*.ko")
 for i in $files; do
     $(pwd)/prebuilts/clang/host/linux-x86/clang-r510928/bin/llvm-strip --strip-unneeded "$i"
     echo "Stripping $i .."
